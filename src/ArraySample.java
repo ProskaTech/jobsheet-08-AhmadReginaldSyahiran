ba374f37fb63390d1697dbeb4e14a533f2ca24b0
AhmadReginaldSyahiran <112646010+AhmadReginaldSyahiran@users.noreply.github.com>
2022-10-13 20:09:17 +0700

    Add files via upload

---
+++ src/ArraySample.java
@@ -0,0 +1,11 @@
+/*Nama: Ahmad Reginald Syahiran
+ * NIM: 21343039
+ */
+public class ArraySample{
+    public static void main(String[] args) {
+        int[] ages = new int[100];
+        for (int i = 0; i < 100; i++){
+            System.out.print(ages[i]);
+        }
+    }
+}
---
+++ src/HariDalamSeminggu.java
@@ -0,0 +1,13 @@
+/*Nama: Ahmad Reginald Syahiran
+ * NIM: 21343039
+ */
+
+
+public class HariDalamSeminggu {
+    public static void main(String[] args) {
+        String[]  days = {"Monday ", "Tuesday ", "Wednesday ", "Thursday ", "Friday ", "Saturday ", "Monday "};
+        for (int i=0; i<=days.length -1; i++){
+            System.out.print(days[i]);
+        }
+    }
+}
